db24cb499cd2636ae44c191a003d976d60f45b65
yuintei <yuin0106@gmail.com>
2022-08-27 03:20:57 +0900

    first commit

---
+++ bolt.js
@@ -0,0 +1,59 @@
+// Description:
+//   TODO を管理できるボットです
+// Commands:
+//   ボット名 add      - TODO を作成
+//   ボット名 done     - TODO を完了にする
+//   ボット名 del      - TODO を消す
+//   ボット名 list     - TODO の一覧表示
+//   ボット名 donelist - 完了した TODO の一覧表示
+
+'use strict';
+const bolt = require('@slack/bolt');
+const dotenv = require('dotenv');
+dotenv.config();
+const todo = require('todo');
+
+const app = new bolt.App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+  logLevel: 'debug'
+});
+
+app.message(/add (.+)/i, ({context, say}) => {
+    const taskName = context.matches[1].trim();
+    todo.add(taskName);
+    say(`追加しました: ${taskName}`);
+  });
+
+  app.message(/done (.+)/i, ({context, say}) => {
+    const taskName = context.matches[1].trim();
+    todo.done(taskName);
+    say(`完了にしました: ${taskName}`);
+  });
+   
+  app.message(/del (.+)/i, ({context, say}) => {
+    const taskName = context.matches[1].trim();
+    todo.del(taskName);
+    say(`削除しました: ${taskName}`);
+  });
+
+  app.message(/^list/i, ({context, say}) => {
+    const list = todo.list();
+    if (list.length === 0) {
+      say('(TODOはありません)');
+    } else {
+      say(list.join('\n'));
+    }
+  });
+  
+  app.message(/donelist/i, ({context, say}) => {
+    const donelist = todo.donelist();
+    if (donelist.length === 0) {
+      say('(完了したTODOはありません)');
+    } else {
+      say(donelist.join('\n'));
+    }
+  });
+
+app.start();
---
+++ todo/index.js
@@ -0,0 +1,80 @@
+'use strict';
+// { name: タスクの文字列, state: 完了しているかどうかの真偽値 }
+const tasks = [];
+
+
+/**
++ * タスクを追加する
++ * @param {string} taskName
++ */
+function add(taskName) {
+    tasks.push({ name: taskName, state: false });
+  }
+  
+/**
++* タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了したかを返す
++* @param {object} task
++* @return {boolean} 完了したかどうか
++*/
+function isDone(task) {
+    return task.state;
+  }
+
+/**
++* タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了していないかを返す
++* @param {object} task
++* @return {boolean} 完了していないかどうか
++*/
+function isNotDone(task) {
+    return !isDone(task);
+  }
+
+/**
++* タスクの一覧の配列を取得する
++* @return {array}
++*/
+function list() {
+    return tasks
+      .filter(isNotDone)
+      .map(task => task.name);
+  }
+  
+/**
++ * タスクを完了状態にする
++ * @param {string} taskName
++ */
+function done(taskName) {
+    const indexFound = tasks.findIndex(task => task.name === taskName);
+    if (indexFound !== -1) {
+      tasks[indexFound].state = true;
+    }
+  }
+  
+  /**
+  + * 完了済みのタスクの一覧の配列を取得する
+  + * @return {array}
+  + */
+  function donelist() {
+    return tasks
+      .filter(isDone)
+      .map(task => task.name);
+  }
+  
+/**
++ * 項目を削除する
++ * @param {string} taskName
++ */
+function del(taskName) {
+    const indexFound = tasks.findIndex(task => task.name === taskName);
+    if (indexFound !== -1) {
+      tasks.splice(indexFound, 1);
+    }
+  }
+
+  module.exports = {
+    add,
+    list,
+    done,
+    donelist,
+    del
+  };
